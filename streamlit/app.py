e15471515286fb25c7e576245f5431da38f8f424
Aengus <aengusb@protonmail.com>
2025-07-18 16:51:38 +0200

    French UI + functional tweaks

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,13 +1,19 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(
+    page_title="Test de personnalité",
+    page_icon="🥸",
+    layout="centered"
+  )
+
 progress = 1
 progress_bar = st.progress(progress)
-progress_step = 100 // 6
+progress_step = 100 // 5
 
 def display_response(answer, progress):
     if answer:
-        st.write(f"Done! you said {answer}")
+        st.write(f"Compris! Vous avez dit {answer}")
         new_progress = progress + progress_step
         progress_bar.progress(new_progress)
         return new_progress
@@ -15,7 +21,7 @@ def display_response(answer, progress):
         st.write("")
         return 0
 
-q_1 = st.text_input("### What personality type do you think you are?")
+q_1 = st.text_input("### À quel type de personnalité pensez-vous appartenir&nbsp;?")
 progress = display_response(q_1, progress)
 
 q_2 = st.text_input("### Question 2")
@@ -28,9 +34,9 @@ q_4 = st.text_input("### Question 4")
 progress = display_response(q_4, progress)
 
 q_5 = st.text_input("### Question 5")
-progress = display_response(q_5, progress)
+progress = 100 - progress_step
+display_response(q_5, progress)
 
 if q_1 and q_2 and q_3 and q_4 and q_5:
-    progress_bar.progress(100)
-    st.write("### Here is your result:")
+    st.write("### Voilà votre résultat:")
     st.image("src/wzrd.jpg")
